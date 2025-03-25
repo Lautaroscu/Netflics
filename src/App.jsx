6b5184d38eadb00d18416abc04e24611cc7b11d8
Lautaro <scuffilautaro@gmail.com>
2025-03-25 16:33:50 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <Router>
       <div className="app">
-       {isAuthenticated &&  <Header isLoggedin={isAuthenticated} /> }
+       <Header isLoggedin={isAuthenticated} /> 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route  path='/login' element={<LoginPage />}/>

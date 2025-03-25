35ca1f75e429b387bc82e48e4bc1f51a0efd3950
Lautaro <scuffilautaro@gmail.com>
2025-03-25 19:30:36 -0300

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -52,10 +52,10 @@ export function LoginPage({handleAuthenticate}) {
   
   const handleGoogleSuccess = (credentialResponse) => {
     console.log("Credential:", credentialResponse);
-    setToken(credentialResponse.credential); // Token JWT
+    setToken(credentialResponse.access_token); // Token JWT
 
     // Podés decodificarlo si querés ver el usuario (opcional)
-    const base64Url = credentialResponse.credential.split(".")[1];
+    const base64Url = token?.split(".")[1];
     const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
     const userData = JSON.parse(window.atob(base64));
     setUser(userData);
